76d26437a4e2c16de9e9910d58f1374bcc22b77d
ammar_yousuf <ammar.yousuf@apps.net.pk>
2022-01-05 19:34:20 +0500

    base project

---
+++ CICD Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICD_Demo
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            Console.WriteLine("Displaying Message from CI/CD Demo Project");
+        }
+    }
+}
